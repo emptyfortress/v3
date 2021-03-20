6477c52033a018e08afa71eb56e9208d45d8d283
Volkov Alexander <volkov.525@gmail.com>
2021-03-20 19:22:38 +0300

    finish pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,35 @@
 <template lang="pug">
-.cards
-	Card(v-for="starter in starters" :starter="starter" @click="fetchEvolution(starter)")
-		template(v-slot:title) {{ starter.name }} - {{ starter.id }}
-		template(v-slot:content)
-			img(:src="starter.sprite")
-		template(v-slot:description)
-			div(v-for="type in starter.types") {{ type }}
+PokemonCards(:pokemons="starters" 
+	:selectedId="selectedId"
+	@clickedPokemon="fetchEvolution")
 
-.cards
-	Card(v-for="starter in evolution" :starter="starter")
-		template(v-slot:title) {{ starter.name }} - {{ starter.id }}
-		template(v-slot:content)
-			img(:src="starter.sprite")
-		template(v-slot:description)
-			div(v-for="type in starter.types") {{ type }}
+PokemonCards(:pokemons="evolution")
+
 </template>
 
 <script>
 import Card from '@/components/Card.vue'
+import PokemonCards from '@/components/PokemonCards.vue'
 
 const api = 'https://pokeapi.co/api/v2/pokemon'
 const STARTERID = [1, 4, 7]
 
 export default {
-	components: { Card },
+	components: { Card, PokemonCards },
 	data() {
 		return {
 			starters: [],
 			evolution: [],
+			selectedId: null,
 		}
 	},
 	async created() {
 		const starters = await this.fetchData(STARTERID)
 		this.starters = starters
 	},
-	computed: {},
 	methods: {
 		async fetchEvolution(pokemon) {
+			this.selectedId = pokemon.id
 			this.evolution = await this.fetchData([pokemon.id + 1, pokemon.id + 2])
 		},
 		async fetchData(ids) {
@@ -55,11 +48,4 @@ export default {
 }
 </script>
 
-<style scoped lang="scss">
-.cards {
-	display: flex;
-}
-img {
-	width: 100%;
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ src/components/PokemonCards.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+.cards
+	Card(v-for="pokemon in pokemons" 
+		:class="{opace: selectedId && pokemon.id !== selectedId}"
+		:starter="pokemon" 
+		@click="clicked(pokemon)").simple
+		template(v-slot:title) {{ pokemon.name }} # {{ pokemon.id }}
+		template(v-slot:content)
+			img(:src="pokemon.sprite")
+		template(v-slot:description)
+			div(v-for="type in pokemon.types") {{ type }}
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+
+export default {
+	props: {
+		pokemons: Array,
+		selectedId: Number,
+	},
+	data() {
+		return {}
+	},
+	components: { Card },
+	methods: {
+		clicked(pokemon) {
+			this.$emit('clickedPokemon', pokemon)
+		},
+	},
+}
+</script>
+
+<style scoped lang="scss">
+.cards {
+	display: flex;
+}
+img {
+	width: 100%;
+}
+.simple {
+	margin-bottom: 1rem;
+	&:hover {
+		opacity: 1;
+	}
+}
+
+.opace {
+	opacity: 0.4;
+}
+</style>
