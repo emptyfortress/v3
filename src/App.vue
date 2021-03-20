0e867fd1839db5cb10118dc782e3f491d9955403
Volkov Alexander <volkov.525@gmail.com>
2021-03-20 16:21:53 +0300

    cards

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,65 @@
 <template lang="pug">
-.card
-	.title
-	.content
-	.description
+.cards
+	Card(v-for="starter in starters" :starter="starter" @click="fetchEvolution(starter)")
+		template(v-slot:title) {{ starter.name }} - {{ starter.id }}
+		template(v-slot:content)
+			img(:src="starter.sprite")
+		template(v-slot:description)
+			div(v-for="type in starter.types") {{ type }}
+
+.cards
+	Card(v-for="starter in evolution" :starter="starter")
+		template(v-slot:title) {{ starter.name }} - {{ starter.id }}
+		template(v-slot:content)
+			img(:src="starter.sprite")
+		template(v-slot:description)
+			div(v-for="type in starter.types") {{ type }}
 </template>
 
 <script>
+import Card from '@/components/Card.vue'
+
+const api = 'https://pokeapi.co/api/v2/pokemon'
+const STARTERID = [1, 4, 7]
+
 export default {
-	components: {},
+	components: { Card },
 	data() {
-		return {}
+		return {
+			starters: [],
+			evolution: [],
+		}
+	},
+	async created() {
+		const starters = await this.fetchData(STARTERID)
+		this.starters = starters
 	},
 	computed: {},
-	methods: {},
+	methods: {
+		async fetchEvolution(pokemon) {
+			this.evolution = await this.fetchData([pokemon.id + 1, pokemon.id + 2])
+		},
+		async fetchData(ids) {
+			const responses = await Promise.all(
+				ids.map((id) => fetch(`${api}/${id}`))
+			)
+			const data = await Promise.all(responses.map((res) => res.json()))
+			return data.map((datum) => ({
+				id: datum.id,
+				name: datum.name,
+				sprite: datum.sprites.other['official-artwork'].front_default,
+				types: datum.types.map((type) => type.type.name),
+			}))
+		},
+	},
 }
 </script>
 
 <style scoped lang="scss">
-.card {
-	border: 1px solid silver;
-	border-radius: 8px;
-	max-width: 200px;
-	margin: 0 5px;
-	cursor: pointer;
-	box-shadow: 0 1px 3px darkgrey;
-	transition: 0.2s;
-	&:hover {
-		box-shadow: 0 1px 9px darkgrey;
-	}
+.cards {
+	display: flex;
 }
-.title,
-.content,
-.description {
-	padding: 1rem;
-	text-transform: capitalize;
-	text-align: center;
-}
-.title,
-.content {
-	border-bottom: 1px solid silver;
-}
-.title {
-	font-size: 1.2rem;
+img {
+	width: 100%;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+.card
+	.title
+		slot(name="title")
+	.content
+		slot(name="content")
+	.description
+		slot(name="description")
+</template>
+
+<script>
+export default {
+	props: {
+		starter: Object,
+	},
+	data() {
+		return {}
+	},
+}
+</script>
+
+<style scoped lang="scss">
+.card {
+	border: 1px solid silver;
+	border-radius: 8px;
+	max-width: 300px;
+	margin: 0 5px;
+	cursor: pointer;
+	box-shadow: 0 1px 3px darkgrey;
+	transition: 0.2s;
+	&:hover {
+		box-shadow: 0 1px 9px darkgrey;
+	}
+}
+.title,
+.content,
+.description {
+	padding: 1rem;
+	text-transform: capitalize;
+	text-align: center;
+}
+.title,
+.content {
+	border-bottom: 1px solid silver;
+}
+.title {
+	font-size: 1.2rem;
+}
+</style>
