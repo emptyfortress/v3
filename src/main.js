49b3a977fcca6ea23dd0f652ba9cd754062137c1
Volkov Alexander <volkov.525@gmail.com>
2021-03-25 13:23:09 +0300

    vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 // import App from './composition/App.vue'
 // import App from './microblog/App.vue'
-import App from './router/App.vue'
-import { router } from './router/router'
+// import App from './router/App.vue'
+// import { router } from './router/router'
+import App from './vuex/App.vue'
 
 const app = createApp(App)
-app.use(router)
 app.mount('#app')
---
+++ src/vuex/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+h2 Hello
+</template>
+
+<script>
+
+export default {
+	setup() {
+		return {
+
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+</style>
