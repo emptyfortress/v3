41dd2a514ecd1848b147bc377e8594e371d49d35
Volkov Alexander <volkov.525@gmail.com>
2021-04-14 13:13:38 +0300

    make table

--- src/main.js
+++ src/main.js
@@ -3,11 +3,10 @@ import { createApp } from 'vue'
 // import App from './microblog/App.vue'
 // import App from './router/App.vue'
 // import { router } from './router/router'
-import App from './photos/App.vue'
-import { store } from './photos/store'
-import { router } from './photos/router'
+// import App from './photos/App.vue'
+// import { store } from './photos/store'
+// import { router } from './photos/router'
+import App from './table/App.vue'
 
 const app = createApp(App)
-app.use(store)
-app.use(router)
 app.mount('#app')
